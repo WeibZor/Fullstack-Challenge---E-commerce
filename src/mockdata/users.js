df4b70787b8be97509501fb9936c48997607f418
WeibZor <ivesmolag@gmail.com>
2026-05-11 17:27:12 -0500

    Commit 15: Creación de mockdata de usuarios

---
+++ src/mockdata/users.js
@@ -0,0 +1,14 @@
+export const mockUsers = [
+  {
+    id: 1,
+    name: 'Lorena Maldonado',
+    email: 'lorena@mystore.com',
+    password: 'secret123',
+  },
+  {
+    id: 2,
+    name: 'Javier Soto',
+    email: 'javier@market.com',
+    password: 'ecommerce2024',
+  },
+];
